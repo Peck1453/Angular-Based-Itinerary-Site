ff39f637241055e4aa100326c25d9e7263c62824
Peck1453 <andy@the-pecks.co.uk>
2018-02-14 17:05:11 +0000

    Post 5

--- App/itinerary/itineraryController.js
+++ App/itinerary/itineraryController.js
@@ -1,68 +1,68 @@
-﻿angular.module("itsItinerary").controller("itineraryController", function ($scope)
-{
+﻿angular.module("itsItinerary").controller("itineraryController", function ($scope, $http) {
     $scope.isEditing = false;
-    $scope.init()
 
-    $scope.itineraries = [
-        {
-            id: 1,
-            itiName: "October Business Development trip 2015",
-            destination: "Germany",
-            purpose: "Work",
-            startDate: new Date("2015-10-03"),
-            endDate: new Date("2015-10-10")
-        },
-        {
-            id: 2,
-            itiName: "November Client-site visit 2015",
-            destination: "America",
-            purpose: "Work",
-            startDate: new Date("2015-11-05"),
-            endDate: new Date("2015-11-08")
-        },
-        {
-            id: 3,
-            itiName: "January Scoping visit 2016",
-            destination: "China",
-            purpose: "Work",
-            startDate: new Date("2016-01-15"),
-            endDate: new Date("2016-01-23")
-        },
-        {
-            id: 4,
-            itiName: "May Recruitment visit 2016",
-            destination: "India",
-            purpose: "Work",
-            startDate: new Date("2016-05-21"),
-            endDate: new Date("2016-05-30")
-        },
-        {
-            id: 5,
-            itiName: "July Conversion visit 2016",
-            destination: "Germany",
-            purpose: "Work",
-            startDate: new Date("2016-07-08"),
-            endDate: new Date("2015-07-15")
-        }
-    ]; 
 
-    $scope.add = function () 
-        {
+
+
+    //$scope.itineraries = [
+    //    {
+    //        id: 1,
+    //        itiName: "October Business Development trip 2015",
+    //        destination: "Germany",
+    //        purpose: "Work",
+    //        startDate: new Date("2015-10-03"),
+    //        endDate: new Date("2015-10-10")
+    //    },
+    //    {
+    //        id: 2,
+    //        itiName: "November Client-site visit 2015",
+    //        destination: "America",
+    //        purpose: "Work",
+    //        startDate: new Date("2015-11-05"),
+    //        endDate: new Date("2015-11-08")
+    //    },
+    //    {
+    //        id: 3,
+    //        itiName: "January Scoping visit 2016",
+    //        destination: "China",
+    //        purpose: "Work",
+    //        startDate: new Date("2016-01-15"),
+    //        endDate: new Date("2016-01-23")
+    //    },
+    //    {
+    //        id: 4,
+    //        itiName: "May Recruitment visit 2016",
+    //        destination: "India",
+    //        purpose: "Work",
+    //        startDate: new Date("2016-05-21"),
+    //        endDate: new Date("2016-05-30")
+    //    },
+    //    {
+    //        id: 5,
+    //        itiName: "July Conversion visit 2016",
+    //        destination: "Germany",
+    //        purpose: "Work",
+    //        startDate: new Date("2016-07-08"),
+    //        endDate: new Date("2015-07-15")
+    //    }];
+
+
+    $scope.add = function () {
         var itineraryDetails = {
-            id: $scope.itineraries.length +1,
+            id: $scope.itineraries.length + 1,
             itiName: $scope.itineraryName,
             destination: $scope.itineraryDestination,
             purpose: $scope.itineraryPurpose,
             startDate: new Date($scope.itineraryStartDate),
             endDate: new Date($scope.itineraryEndDate)
         };
-        
+
 
 
         $scope.itineraries.push(itineraryDetails);
         $scope.isEditing = false;
 
-        
+
     };
 
 
@@ -78,8 +78,7 @@
     };
 
 
-    $scope.remove = function (itineraryId)
-    {
+    $scope.remove = function (itineraryId) {
         var itineraryToRemove = $scope.itineraries.indexOf(itineraryId);
         $scope.itineraries.splice(itineraryToRemove, 1);
         $scope.isEditing = false;
@@ -89,10 +88,17 @@
 
 
     }
+    $scope.init = function () {
 
+        $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/its/")
+            .success(function (response) {
+                $scope.itineraries = response;
+            })
+            .error(function (error) {
+                $scope.errorMessage = error;
+            });
+    }
+
+    $scope.init();
 
 });
-
-
-
-
